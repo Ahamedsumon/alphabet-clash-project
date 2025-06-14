0bd4ad13ea0725d791cd2b062fbed9019471ec11
Ahamed Sumon <ahamedsumon13579@gmail.com>
2025-06-14 15:40:50 +0300

    added home section, play ground sectionn and Score section and also added Javascript function

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function addBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400')
+}
+
+
+
+function getRandomAlphabet(){
+    const randomNumber = Math.random()*25;
+        const roundRandomNumber = Math.round(randomNumber);
+        const alphabet = "abcdefghijklmnopqrstuvwxyz/";
+        const alphabetIntoArray = alphabet.split('');
+        const randomDisplayText = alphabetIntoArray[roundRandomNumber];
+        const displayText = document.getElementById('display-text');
+        displayText.innerText = randomDisplayText.toUpperCase();
+        addBackgroundColorById(randomDisplayText)
+}
+
+
+
+
+document.getElementById('play-button').addEventListener('click', function(){
+            const hidingGameStartSection = document.getElementById('home-section');
+            hidingGameStartSection.classList.add('hidden');
+
+
+            const unHidingPlayGroundSection = document.getElementById('play-ground-section');
+            unHidingPlayGroundSection.classList.remove('hidden');
+
+            getRandomAlphabet();
+            
+        })
